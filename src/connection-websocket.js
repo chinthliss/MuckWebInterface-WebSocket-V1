1234ecdf01deac71d670a88f564c134bfc92eadd
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-06-17 17:10:06 +0100

    Continued reworks - Redid WebSocket handlers
    Including reimplementing the previous message buffer.

--- src/connection-websocket.js
+++ src/connection-websocket.js
@@ -43,6 +43,12 @@ export default class ConnectionWebSocket extends Connection {
      */
     ensureConnectionTimeout = -1;
 
+    /**
+     * Used to hold messages that try to send before the initial connection is complete
+     * @type {string[]}
+     */
+    connectingOutgoingMessageBuffer;
+
     constructor(context, core) {
         super(context, core);
 
@@ -66,6 +72,30 @@ export default class ConnectionWebSocket extends Connection {
 
     }
 
+    /**
+     * @param {CloseEvent} e
+     */
+    handleWebSocketClose = (e) => {
+        if (this.core.debug) console.log("WebSocket closed: ", e);
+        this.core.connectionFailed("Websocket closed with reason: " + e.reason);
+    };
+
+    /**
+     * @param {ErrorEvent} e
+     */
+    handleWebSocketError = (e) => {
+        console.log("Mwi-Websocket Error - WebSocket error: ", e);
+        this.core.connectionFailed("Websocket returned error: " + e.message);
+    }
+
+    /**
+     * @param {MessageEvent} e
+     */
+    handleWebSocketMessage = (e) => {
+        let message = e.data.slice(0, -2); //Remove \r\n
+        this.core.receivedString(message);
+    }
+
     openWebsocket(websocketToken) {
         this.connection = new WebSocket(this.websocketUrl, 'mwi');
 
@@ -74,24 +104,20 @@ export default class ConnectionWebSocket extends Connection {
             this.core.updateAndDispatchStatus(Core.connectionStates.login);
             this.receivedWelcome = false;
             this.handshakeCompleted = false;
+            this.connectingOutgoingMessageBuffer = [];
             this.ensureConnectionTimeout = setTimeout(function () {
                 console.log("Mwi-Websocket Error - WebSocket took too long to complete handshake, assuming failure.");
                 this.core.connectionFailed("Websocket took too long to connect.");
             }.bind(this), 10000);
         };
 
-        this.connection.onclose = () => {
-            if (this.core.debug) console.log("WebSocket closed.");
-            this.core.connectionFailed("Websocket closed unexpectedly.")
-        };
+        this.connection.onclose = this.handleWebSocketClose;
+        this.connection.onerror = this.handleWebSocketError;
 
-        this.connection.onerror = (e) => {
-            console.log("Mwi-Websocket Error - WebSocket error: ", e);
-            this.core.connectionFailed("Websocket return error: " + e);
-        };
-
+        // During connection, we use a special onMessage handling to deal with the handshake
         this.connection.onmessage = (e) => {
             let message = e.data.slice(0, -2); //Remove \r\n
+
             if (!this.receivedWelcome) {
                 if (message === 'welcome') {
                     this.connection.send('auth ' + websocketToken);
@@ -100,18 +126,27 @@ export default class ConnectionWebSocket extends Connection {
                 } else console.log("Mwi-Websocket Error - WebSocket got an unexpected message whilst expecting welcome: " + message);
                 return;
             }
+
             if (!this.handshakeCompleted) {
                 if (message.startsWith('session ')) {
                     if (this.core.debug) console.log("WebSocket received session.");
-                    this.handshakeCompleted = true;
                     let session = message.slice(8);
+                    clearTimeout(this.ensureConnectionTimeout);
+                    this.handshakeCompleted = true;
+                    this.connection.onmessage = this.handleWebSocketMessage;
                     this.core.updateAndDispatchSession(session);
                     this.core.updateAndDispatchStatus(Core.connectionStates.connected);
-                    clearTimeout(this.ensureConnectionTimeout);
+                    //Resend anything that was buffered
+                    for (let i = 0; i++; i < this.connectingOutgoingMessageBuffer.length) {
+                        this.sendString(this.connectingOutgoingMessageBuffer[i]);
+                    }
+                    this.connectingOutgoingMessageBuffer = [];
+
                 } else console.log("Mwi-Websocket Error - WebSocket got an unexpected message whilst expecting session: " + message);
                 return;
             }
-            this.core.receivedString(message);
+            console.log("Unexpected message during login: " + message);
+
         }
     }
 
@@ -139,6 +174,11 @@ export default class ConnectionWebSocket extends Connection {
     sendString(stringToSend) {
         if (stringToSend.length > 30000) {
             console.log("Websocket connection - had to abort sending a strings because it's over 30,000 characters.");
+            return;
+        }
+        // Buffer the string if we're still connecting
+        if (!this.receivedWelcome || !this.handshakeCompleted) {
+            if (this.core.debug) console.log("Buffering outgoing message: ", stringToSend);
             return;
         }
         this.connection.send(stringToSend);
